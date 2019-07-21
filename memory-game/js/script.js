01f50b945db88c4b790fd8388b60df6838622b9b
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-20 22:15:27 -0300

    memory-game / store cards

--- memory-game/js/script.js
+++ memory-game/js/script.js
@@ -1,7 +1,25 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
   this.classList.toggle('flip');
+
+  if(!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    // do cards match?
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
