5beb00e5e0d23a20022bea673bc39e0ff826fc89
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-21 22:20:46 -0300

    memory-game / lock boards

--- memory-game/js/script.js
+++ memory-game/js/script.js
@@ -1,9 +1,11 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
   this.classList.toggle('flip');
 
   if(!hasFlippedCard) {
@@ -33,9 +35,13 @@ function disableCards() {
 }
 
 function unflipCards() {
+  lockBoard = true;
+
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+    
+    lockBoard = false;
   }, 1500);
 }
 
