739326f8c0ab8f3e11e5e6195d8476a10075987e
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-09 10:27:57 -0300

    add: editTodo implemented

--- todoapp-angular/src/app/components/todo-list/todo-list.component.ts
+++ todoapp-angular/src/app/components/todo-list/todo-list.component.ts
@@ -18,22 +18,22 @@ export class TodoListComponent implements OnInit {
     this.todoTitle = '';
     this.todos = [
       {
-        'id': 1,
-        'title': 'Finish Angular Screencast',
-        'completed': false,
-        'editing': false,
+        id: 1,
+        title: 'Finish Angular Screencast',
+        completed: false,
+        editing: false,
       },
       {
-        'id': 2,
-        'title': 'Take over world',
-        'completed': false,
-        'editing': false,
+        id: 2,
+        title: 'Take over world',
+        completed: false,
+        editing: false,
       },
       {
-        'id': 3,
-        'title': 'One more thing',
-        'completed': false,
-        'editing': false,
+        id: 3,
+        title: 'One more thing',
+        completed: false,
+        editing: false,
       },
     ];
   }
@@ -52,6 +52,14 @@ export class TodoListComponent implements OnInit {
 
     this.todoTitle = '';
     this.id++;
+  }
+
+  editTodo(todo: Todo): void {
+    todo.editing = true;
+  }
+
+  doneEdit(todo: Todo): void {
+    todo.editing = false;
   }
 
   deleteTodo(id: number): void {
