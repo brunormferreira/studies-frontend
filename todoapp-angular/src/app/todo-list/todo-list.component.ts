a5c16503f5a01aeebda332bf4ac4235ad1922cf5
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-08 23:28:44 -0300

    add: some layout styles and addTodo function

--- todoapp-angular/src/app/todo-list/todo-list.component.ts
+++ todoapp-angular/src/app/todo-list/todo-list.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  todos: object[];
+  todoTitle: string;
+  id: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.id = 4;
+    this.todoTitle = '';
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Finish Angular Screencast',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'Take over world',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'One more thing',
+        'completed': false,
+        'editing': false,
+      },
+    ];
   }
 
+  addTodo(): void {
+    if (this.todoTitle.trim().length === 0) {
+      return;
+    }
+
+    this.todos.push({
+      id: this.id,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    });
+
+    this.todoTitle = '';
+    this.id++;
+  }
 }
