345b4909fab6eb2ba5fdbf4a74c26d8ce0ee34f7
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-04 10:58:05 -0300

    fix: some styles and details

--- drinkbeers/src/app/mock-beers.ts
+++ drinkbeers/src/app/mock-beers.ts
@@ -1,7 +1,7 @@
 import { Beer } from './beer';
 
 export const BEERS: Beer[] = [
-  { id: 11, type: 'Pale-Ale', brand: 'Colorado' },
+  { id: 11, type: 'PaleAle', brand: 'Colorado' },
   { id: 12, type: 'Pilsen', brand: 'Tupiniquim' },
   { id: 13, type: 'IPA', brand: 'Roleta Russa' },
   { id: 14, type: 'Pilsen', brand: 'Serramalte' },
@@ -11,4 +11,5 @@ export const BEERS: Beer[] = [
   { id: 18, type: 'Premium', brand: 'Heineken' },
   { id: 19, type: 'Pilsen', brand: 'Budweiser' },
   { id: 20, type: 'IPA', brand: 'Krug Rancor' },
-]
+];
+
