77dbb575b7c6ec3c5df2235a57282b3ffd9bea0a
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-05 14:35:54 -0300

    add: top5, some styles and more routes with patchFull

--- drinkbeers/src/app/app-routing.module.ts
+++ drinkbeers/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { BeersComponent } from '../app/components/beers/beers.component';
+import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'beers', component: BeersComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'beers', component: BeersComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
--- drinkbeers/src/app/components/dashboard/dashboard.component.ts
+++ drinkbeers/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Beer } from 'src/app/beer';
+import { BeerService } from 'src/app/services/beer.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  beers: Beer[] = [];
 
-  constructor() { }
+  constructor(private beerService: BeerService) { }
 
   ngOnInit() {
+    this.getBeers();
+  }
+
+  // To take top 5
+  getBeers(): void {
+    this.beerService.getBeers()
+      .subscribe(beers => this.beers = beers.slice(1, 5));
   }
 
 }
