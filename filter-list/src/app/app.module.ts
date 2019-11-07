111e912b42c8bb514648173a1fc0a211aaae2b99
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-07 15:36:17 -0300

    add: header and footer with different styles to push them to your position

--- filter-list/src/app/app.module.ts
+++ filter-list/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilterPipe } from './pipe/filter.pipe';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilterPipe
+    FilterPipe,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
