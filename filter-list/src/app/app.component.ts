4effcceeecf1611e630b8275d36343adaaf095e9
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-07 14:59:20 -0300

    add/start: filter pipe on input

---
+++ filter-list/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'filter-list';
+
+  list = [
+    'Igor',
+    'Maria',
+    'Joao',
+    'Helena',
+    'Bruno',
+    'Renato',
+    'Renata'
+  ];
+}
+
